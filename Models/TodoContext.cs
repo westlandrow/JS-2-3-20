b1e3009932cb0ade575497ffd13d4d23b1aab473
Westland Row <westlandRow@gmail.com>
2020-03-02 23:23:23 +0000

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webAPI22_feb20.Models;
+
+namespace webAPI22_feb20.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
